7c0da0391a4f80d4586c1836dee4ad0648a1b900
Georgette KPEGBA <kpegbageorgette@hotmail.com>
2024-11-02 11:54:47 +0100

    slider

--- scripts/pages/photographerPage.js
+++ scripts/pages/photographerPage.js
@@ -1,19 +1,18 @@
 // Mettre le code JavaScript lié à la page photographer.html
-import { displayModal, closeModal } from '../utils/contactForm.js';
-import { getMedia } from '../api/media.js';
-import { photographerMainTemplate } from '../templates/photographerMainTemplate.js';
-import { mediaFactory } from '../factory/mediaFactory.js';
-import { displayLightBox } from '../templates/displayLightBox.js';
-import { displayCounterInfos } from '../templates/displaycounterInfos.js';
+import { displayModal, closeModal } from "../utils/contactForm.js";
+import { getMedia } from "../api/media.js";
+import { photographerMainTemplate } from "../templates/photographerMainTemplate.js";
+import { mediaFactory } from "../factory/mediaFactory.js";
+import { displayLightBox } from "../templates/displayLightBox.js";
+import { displayCounterInfos } from "../templates/displaycounterInfos.js";
 // @event
 const contactFormBtn = document.querySelector(".contact_button");
 const contactFormClose = document.querySelector(".contact_close_btn");
-// modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-contactFormBtn.addEventListener('click', displayModal);
+contactFormBtn.addEventListener("click", displayModal);
 contactFormClose.addEventListener("click", closeModal);
 // get every photographer id
 const url = new URL(window.location.href);
-const id = url.searchParams.get('id');
+const id = url.searchParams.get("id");
 // diplay pfotographer
 function displayPhotographerSinglePage(photographer) {
     const photographersSection = document.querySelector("main");
@@ -21,82 +20,43 @@ function displayPhotographerSinglePage(photographer) {
     const userCardDOM = photographerModel.getUserCardDOM();
     photographersSection.appendChild(userCardDOM);
 }
+
 // factory media
 function displayPhotographerMedia(mediaList) {
-    const photographersGallery = document.querySelector(".photograph-galery_content");
-    mediaList.forEach(media => {
+    const photographersGallery = document.querySelector(
+        ".photograph-galery_content"
+    );
+    mediaList.forEach((media) => {
         const photographerMedia = mediaFactory(media);
         const mediaCardDOM = photographerMedia.getMediaCardDOM();
         photographersGallery.appendChild(mediaCardDOM);
     });
-    const allMedia = document.querySelectorAll('.photographer-all-img');
+    const allMedia = document.querySelectorAll(".photographer-all-img");
     allMedia.forEach((media, index) => {
-        media.addEventListener('click', (event) => {
-            console.log('toto', event.target);
-            console.log('image', mediaList[index]);
-            displayLightBox(mediaList[index]);
-            const lightBoxMedia = displayLightBox(mediaList[index]);
+        media.addEventListener("click", (event) => {
+            console.log("toto", event.target);
+            console.log("image", mediaList[index]);
+            const lightBoxMedia = displayLightBox(index, mediaList);
             const mediaCardDOM = lightBoxMedia.getMediaCardDOM();
-            const lightboxWrapper = document.querySelector('.lightbox_wrapper');
+            const lightboxWrapper = document.querySelector(".lightbox_wrapper");
             lightboxWrapper.appendChild(mediaCardDOM);
-            // console.log(lightBoxMedia);
-            // console.log(lightboxWrapper);
-            // const btnNext = document.querySelector('.btn_next');
-            // const btnPrevious = document.querySelector('.btn_previous');
-            // btnNext.addEventListener('click', (event) => {
-            //     const card = document.querySelector('.Next media');
-            // })
-
-
-        }
-        )
-    }
-
-    )
-
+        });
+    });
 }
-// display lightbox on click on the card
-// function displayPhotograherZoom(media) {
-//     // displayPhotograherZoom(media);
-//     const lightBox = document.querySelector(".lightbox_wrapper");
-//     // find the media in the array
-//     // const mediaId = media.dataset.media;
-//     // const mediaClick = photographerMedia.find(media => media.id === mediaId);
-//     media.forEach(media => {
-//         const photographerMedia = displayLightBox(media);
-//         const mediaCardDOM = photographerMedia.getMediaCardDOM();
-//         lightBox.appendChild(mediaCardDOM);
-//     });
-//     console.log(lightBox);
-//     // const lightBoxClose = document.querySelector(".lbtn_close_lightbox btn_close");
-//     // lightBoxClose.addEventListener("click", () => {
-//     //     lightBox.classList.remove("open");
-//     // });
-// }
 
-//
-function displayPhotographerCounter(price,) {
+function displayPhotographerCounter(price) {
     displayCounterInfos(price);
     const photographersSection = document.querySelector("main");
     const counterInfos = displayCounterInfos(price);
     const userCardDOM = counterInfos.getCardDOM();
     photographersSection.appendChild(userCardDOM);
 }
-// function displayPhotographerCounter(totalMediaLikeCountrice) {
-//     displayImage(photographerMedia);
-//     const photographersSection = document.querySelector("main");
-//     const counterInfos = displayCounterInfos(price);
-//     const userCardDOM = counterInfos.getCardDOM();
-//     photographersSection.appendChild(userCardDOM);
-// }
-
 
 async function init() {
     // Récupère les datas des photographes
     const { photographer, photographerMedia } = await getMedia(id);
     displayPhotographerSinglePage(photographer);
-    displayPhotographerMedia(photographerMedia)
-    // displayPhotograherZoom(photographerMedia)
+    displayPhotographerMedia(photographerMedia);
     displayPhotographerCounter(photographer.price);
 }
 init();
--- scripts/templates/displayLightBox.js
+++ scripts/templates/displayLightBox.js
@@ -1,8 +1,11 @@
 // media sections  
-export function displayLightBox(photographerMedia) {
-    const { image, id, mediaId, photographerId } = photographerMedia;
-    const picture = `assets/media/${photographerId}/${image}`;
+export function displayLightBox(index, mediaArray) {
     function getMediaCardDOM() {
+        let currentIndex = index;
+        let photographerMedia = mediaArray[currentIndex];
+        const { image, video, photographerId } = photographerMedia;
+        const picture = `assets/media/${photographerId}/${image}`;
+        const movie = `assets/media/${photographerId}/${video}`;
         const card = document.createElement('div');
         card.classList.add('photographer-media');
         const media = document.createElement('div');
@@ -13,21 +16,38 @@ export function displayLightBox(photographerMedia) {
         const img = document.createElement('img');
         img.src = picture;
         img.classList.add("photographer-all-img");
+        // video
+        const videoCard = document.createElement('video');
+        videoCard.src = movie;
+        videoCard.classList.add("photographer-all-video");
         // svg
         const btnClose = document.querySelector('.btn_close');
+        const btnNext = document.querySelector('.btn_next');
+        const btnPrevious = document.querySelector('.btn_previous');
         // access to the lightbox
         // add event listener to open the lightbox
-
         const lightboxWrapper = document.querySelector(".lightbox_wrapper");
         lightboxWrapper.style.display = 'flex';
-
         lightboxWrapper.appendChild(card);
         card.appendChild(media);
         media.appendChild(img);
+        media.appendChild(videoCard);
         lightboxWrapper.appendChild(btnClose);
         btnClose.addEventListener('click', function () {
             lightboxWrapper.style.display = 'none';
             card.remove();
+        });
+        lightboxWrapper.appendChild(btnNext);
+        btnNext.addEventListener('click', function () {
+            currentIndex = currentIndex + 1;
+            console.log('next');
+            photographerMedia = mediaArray[currentIndex];
+            console.log(photographerMedia);
+            img.src = `assets/media/${photographerMedia.photographerId}/${photographerMedia.image}`;
+        });
+        lightboxWrapper.appendChild(btnPrevious);
+        btnPrevious.addEventListener('click', function () {
+            console.log('previous');
         });
         return card;
     }
